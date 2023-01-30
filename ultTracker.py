1367aa94ed9e93bbb3de927f8f45bbf2f4ba0437
Capta <Captaincow285@gmail.com>
2023-01-29 22:57:07 -0500

    fixed red vs green

--- ultTracker.py
+++ ultTracker.py
@@ -7,16 +7,34 @@ import openpyxl
 
 pytesseract.pytesseract.tesseract_cmd=r'C:\Users\Capta\Documents\Tesseract\tesseract.exe'
 
-table = [["map", "round", "team1name", "team1ults","team2name", "team2ults", "winningTeam"]]
-ultGreen = []
-ultRed = []
-playersGreen = []
-playersRed = []
+table = [["map", "round", "team1name", "team1eco", "team1ults","team2name", "team2eco", "team2ults", "winningTeam"]]
+ultLeft = []
+ultRight = []
+playersLeft = []
+playersRight = []
 topBarText = []
+roundsWorkheet = None
 
 def readRibScrape(filepath):
     excelFile = openpyxl.load_workbook(filepath)
-    
+    roundsWorksheet = excelFile('WIN CONDITIONS')
+    infoWorksheet = excelFile('PLAYERS INFO')
+
+    matchinfo = []
+    selectedData  = []
+    for row in infoWorksheet:
+        selectedData.append([row[2].value, row[3].value])
+        selectedData.append([row[6].value, row[7].value])
+        selectedData.append([row[10].value, row[11].value])
+        selectedData.append([row[14].value, row[15].value])
+        selectedData.append([row[16].value, row[17].value])
+        selectedData.append([row[20].value, row[21].value])
+        selectedData.append([row[24].value, row[25].value])
+        selectedData.append([row[28].value, row[29].value])
+        selectedData.append([row[32].value, row[33].value])
+        selectedData.append([row[36].value, row[37].value])
+        matchinfo.append(selectedData)
+
 
 def readImage(path): #/TODO: turn the arrays into a return statement instead of global variables
     # load and resize the image
@@ -60,20 +78,21 @@ def readImage(path): #/TODO: turn the arrays into a return statement instead of 
     ultXValues = [430, 1490] 
     ultYValues = [800, 864, 928, 992, 1056]
 
-    greenValues = []
-    redValues = []
+    leftValues = []
+    rightValues = []
 
     for yCoord in ultYValues:
-        greenValues.append(image[yCoord, ultXValues[0]])
-        redValues.append(image[yCoord, ultXValues[1]])
+        leftValues.append(image[yCoord, ultXValues[0]])
+        rightValues.append(image[yCoord, ultXValues[1]])
 
 
     #/TODO: When iterating, these will need to be duplicated outside of the loop to expose them
-    ultGreen = []
-    ultRed = []
-    playersGreen = []
-    playersRed = []
+    ultLeft = []
+    ultRight = []
+    playersLeft = []
+    playersRight = []
     topBarText = []
+    loadoutValues = [] #Green value, Red value
 
     """"
     Player name dimensions:
@@ -85,64 +104,87 @@ def readImage(path): #/TODO: turn the arrays into a return statement instead of 
     playerNameXValues = [95, 1700]
 
     for yCoord in playerNameYValues:
-        greenCrop = OCRImage[yCoord:(yCoord + 30), playerNameXValues[0]:(playerNameXValues[0] + 125)]
-        greenCrop = cv2.resize(greenCrop, (250, 60))
-        greenCrop = cv2.threshold(greenCrop, 160, 255, cv2.THRESH_BINARY)
-        greenCrop = greenCrop[1]
-        greenCrop = cv2.erode(greenCrop, kernel, iterations=2)
-        greenCrop = cv2.dilate(greenCrop, kernel, iterations=1)
-        name = pytesseract.image_to_string(greenCrop, config= "-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz --psm 8")
-        playersGreen.append(name.rstrip('\n'))
+        leftCrop = OCRImage[yCoord:(yCoord + 30), playerNameXValues[0]:(playerNameXValues[0] + 125)]
+        leftCrop = cv2.resize(leftCrop, (250, 60))
+        leftCrop = cv2.threshold(leftCrop, 160, 255, cv2.THRESH_BINARY)
+        leftCrop = leftCrop[1]
+        leftCrop = cv2.erode(leftCrop, kernel, iterations=2)
+        leftCrop = cv2.dilate(leftCrop, kernel, iterations=1)
+        name = pytesseract.image_to_string(leftCrop, config= "-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz --psm 8")
+        playersLeft.append(name.rstrip('\n'))
 
-        redCrop = OCRImage[yCoord:(yCoord + 30), playerNameXValues[1]:(playerNameXValues[1] + 125)]
-        redCrop = cv2.resize(redCrop, (250, 60))
-        redCrop = cv2.threshold(redCrop, 160, 255, cv2.THRESH_BINARY)
-        redCrop = redCrop[1]
-        # redCrop = cv2.fastNlMeansDenoising(redCrop,None,10,10,7,21)
-        redCrop = cv2.erode(redCrop, kernel, iterations=1)
-        #redCrop = cv2.dilate(redCrop, kernel, iterations=1)
-        # cv2.imshow("image", redCrop)
-        # cv2.waitKey(0)
-        name = pytesseract.image_to_string(redCrop, config= "-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz --psm 8")
-        playersRed.append(name.rstrip('\n'))
+        rightCrop = OCRImage[yCoord:(yCoord + 30), playerNameXValues[1]:(playerNameXValues[1] + 125)]
+        rightCrop = cv2.resize(rightCrop, (250, 60))
+        rightCrop = cv2.threshold(rightCrop, 160, 255, cv2.THRESH_BINARY)
+        rightCrop = rightCrop[1]
+        rightCrop = cv2.erode(rightCrop, kernel, iterations=1)
+        rightCrop = cv2.dilate(rightCrop, kernel, iterations=1)
+        name = pytesseract.image_to_string(rightCrop, config= "-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz --psm 8")
+        playersRight.append(name.rstrip('\n'))
 
-    for pixelGreen in greenValues:
-        isGreen = True
-        for i in range(len(pixelGreen)):
-            isGreen = isGreen and (pixelGreen[i] > colorBoundries[0][0][i] and pixelGreen[i] < colorBoundries[0][1][i])
+    isGreen = True
+    checkedPixel = image[700, 110]
+    vibrantGreenBoundries = [[112, 144, 48], [128, 176, 64]]
+    for i in range(len(checkedPixel)):
+        isGreen = isGreen and (checkedPixel[i] > vibrantGreenBoundries[0][i] and checkedPixel[i] < vibrantGreenBoundries[1][i])
+    #print(checkedPixel)
+    #print(isGreen)
+
+    for leftPixel in leftValues:
+        selectedColor = None
         if isGreen:
-            ultGreen.append(True)
+            selectedColor = colorBoundries[0]
         else:
-            ultGreen.append(False)
+            selectedColor = colorBoundries[1]
+        isCorrectColor = True
+        for i in range(len(leftPixel)):
+            isCorrectColor = isCorrectColor and (leftPixel[i] > selectedColor[0][i] and leftPixel[i] < selectedColor[1][i])
+        if isCorrectColor:
+            ultLeft.append(True)
+        else:
+            ultLeft.append(False)
 
-    for pixelRed in redValues:
-        isRed = True
-        for i in range(len(pixelRed)):
-            isRed = isRed and (pixelRed[i] > colorBoundries[1][0][i] and pixelRed[i] < colorBoundries[1][1][i])
-        if isRed:
-            ultRed.append(True)
+    for rightPixel in rightValues:
+        selectedColor = None
+        if isGreen:
+            selectedColor = colorBoundries[1]
         else:
-            ultRed.append(False)
+            selectedColor = colorBoundries[0]
+        isCorrectColor = True
+        for i in range(len(rightPixel)):
+            isCorrectColor = isCorrectColor and (rightPixel[i] > colorBoundries[1][0][i] and rightPixel[i] < colorBoundries[1][1][i])
+        if isCorrectColor:
+            ultRight.append(True)
+        else:
+            ultRight.append(False)
+    
+    # Loadout values: Y 705-740, X green 155-245, X red 1705-1800
+    loadoutValueImage = OCRImage[705:740, 155:245]
+    value = pytesseract.image_to_string(loadoutValueImage, config= "-c tessedit_char_whitelist=01234567890, --psm 8")
+    loadoutValues.append(int(value.replace(",", "")))
+    
+    loadoutValueImage = OCRImage[705:740, 1705:1800]
+    value = pytesseract.image_to_string(loadoutValueImage, config= "-c tessedit_char_whitelist=01234567890, --psm 8")
+    loadoutValues.append(int(value.replace(',', '')))
     
     # topBarText key: team 1, team 1 score, team 2 score, team 2
     topBar = OCRImage[10:60, 630:710]
     topBar = cv2.erode(topBar, kernel, iterations=1)
-    cv2.imshow("images", topBar)
-    cv2.waitKey(0)
     team1Name = pytesseract.image_to_string(topBar, config= "-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8")
     topBarText.append(team1Name.rstrip('\n'))
 
     topBar = OCRImage[5:65, 800:870]
     topBar = cv2.erode(topBar, kernel, iterations=1)
-    # cv2.imshow("images", topBar)
-    # cv2.waitKey(0)
     team1Name = pytesseract.image_to_string(topBar, config= "-c tessedit_char_whitelist=01234567890 --psm 8")
     topBarText.append(team1Name.rstrip('\n'))
 
+    print(loadoutValues)
     print(topBarText)
-    print(playersGreen)
-    print(playersRed)
-    return []
+    print(playersLeft)
+    print(ultLeft)
+    print(playersRight)
+    print(ultRight)
+    #return []
 
 # Row format: "map", "round", "team1name", "team1ults","team2name", "team2ults", "winningTeam"
 def matchNames():
@@ -178,4 +220,4 @@ args = vars(ap.parse_args())
 readImage(args["image"])
 
 # convert into the correct format after getting rib data
-table.append()
+# table.append()
