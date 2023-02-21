3d919e52f3514b8e738845338065bf29a41c54c7
Capta <Captaincow285@gmail.com>
2023-02-21 00:28:08 -0500

    Finished auto screenshot script

--- screenshotTool/screenshotter.py
+++ screenshotTool/screenshotter.py
@@ -6,8 +6,6 @@ import argparse
 import cv2
 import time
 
-counter = 0
-
 """
 pil_image = PIL.ImageGrab.grab(bbox=None, include_layered_windows=False, all_screens=True)
 open_cv_image = np.array(pil_image) 
@@ -17,8 +15,8 @@ cv2.imshow("Image", open_cv_image)
 cv2.waitKey(0)
 """
 
-image = cv2.imread("C9-DRX-M1-R2.jpg")
-image = cv2.resize(image, (1920, 1080))
+# image = cv2.imread("C9-DRX-M1-R2.jpg")
+# image = cv2.resize(image, (1920, 1080))
 
 # Green: X = 110; Y = 650-725 (675, 700); Colors: R 40-56, G 152-176, B 112-120
 # Red: X = 1810: Y = 650-725 (675, 700); Colors: R 208-240, G 64-80, B 88-104
@@ -44,14 +42,19 @@ def checkIsScoreboard(image):
         return False
     return True
 
-
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-if checkIsScoreboard(image):
-    print("Found Scoreboard! Writing.....")
-    cv2.imwrite("generated/image" + str(counter) + ".jpg")
-    counter += 1
-    time.sleep(30)
-else:
-    print("No Scoreboard Found")
-    time.sleep(2)
+counter = 0
+while(True):
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    # cv2.imshow("image", image)
+    # cv2.waitKey(0)
+    if checkIsScoreboard(image):
+        print("Found Scoreboard! Writing.....")
+        filename = "screenshotTool/generated/image" + str(counter) + ".jpg"
+        print(filename)
+        cv2.imwrite(filename, image)
+        counter += 1
+        time.sleep(15)
+    else:
+        print("No Scoreboard Found")
+        time.sleep(1)
